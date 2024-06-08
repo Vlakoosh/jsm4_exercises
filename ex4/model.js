4349397f8b261e7ff11c59e3e9951612851403af
Szymon Walkusz <szymon.walkusz@student.kdg.be>
2024-06-08 13:51:49 +0200

    Conway's game of life

--- ex4/model.js
+++ ex4/model.js
@@ -4,53 +4,61 @@ const HEIGHT = 20;
 const WIDTH = 30;
 
 let grid = new Array(HEIGHT)
-for (let i = 0; i < grid.length; i++) {
-    grid[i] = new Array(WIDTH).fill(false);
-}
-let nextGrid = new Array(HEIGHT).fill(new Array(WIDTH).fill(false));
-for (let i = 0; i < grid.length; i++) {
-    next[i] = new Array(WIDTH).fill(false);
-}
+setEmptyGrid(grid);
+randomizeGrid(grid);
 
-for (let row = 0; row < HEIGHT; row++){
-    for (let col = 0; col < WIDTH; col++){
-        grid[row][col] = Math.random() > 0.6;
+let nextGrid = new Array(HEIGHT);
+setEmptyGrid(nextGrid);
+
+function setEmptyGrid(gridParam) {
+    for (let i = 0; i < gridParam.length; i++) {
+        gridParam[i] = new Array(WIDTH).fill(false);
     }
 }
 
-export function next() {
+function randomizeGrid(gridParam) {
+    for (let row = 0; row < HEIGHT; row++) {
+        for (let col = 0; col < WIDTH; col++) {
+            gridParam[row][col] = Math.random() > 0.6;
+        }
+    }
+}
+
+
+function next() {
     //clean the nextGrid 2d array
-    for (let i = 0; i < grid.length; i++) {
-        nextGrid[i] = new Array(WIDTH).fill(false);
-    }
-    console.log()
+    setEmptyGrid(nextGrid);
 
-    for (let row = 0; row < HEIGHT; row++){
-        for (let col = 0; col < WIDTH; col++){
+    //count the neighbors of each cell
+    for (let row = 0; row < HEIGHT; row++) {
+        for (let col = 0; col < WIDTH; col++) {
             let neighbors = 0;
-            if (col > 0 && row > 0) neighbors += Number(grid[row-1][col-1]);
-            if (col > 0) neighbors += Number(grid[row][col-1]);
-            if (row > 0) neighbors += Number(grid[row-1][col]);
-            if (col < WIDTH - 1 && row < HEIGHT - 1) neighbors += Number(grid[row+1][col+1]);
-            if (col < WIDTH - 1) neighbors += Number(grid[row][col+1]);
-            if (row < HEIGHT - 1) neighbors += Number(grid[row+1][col]);
+            if (col > 0 && row > 0) neighbors += Number(grid[row - 1][col - 1]);
+            if (col > 0) neighbors += Number(grid[row][col - 1]);
+            if (row > 0) neighbors += Number(grid[row - 1][col]);
+            if (col < WIDTH - 1 && row < HEIGHT - 1) neighbors += Number(grid[row + 1][col + 1]);
+            if (col < WIDTH - 1) neighbors += Number(grid[row][col + 1]);
+            if (row < HEIGHT - 1) neighbors += Number(grid[row + 1][col]);
 
-            if (grid[row][col] === false && neighbors === 3){
+            if (grid[row][col] === false && neighbors === 3) {
                 nextGrid[row][col] = true;
             }
-            if (grid[row][col] === true && neighbors >= 2 && neighbors <= 3){
+            if (grid[row][col] === true && neighbors >= 2 && neighbors <= 3) {
                 nextGrid[row][col] = false;
             }
-            if (grid[row][col] === true && neighbors >= 2 && neighbors <= 3){
+            if (grid[row][col] === true && neighbors >= 2 && neighbors <= 3) {
                 nextGrid[row][col] = true;
             }
         }
     }
-    for (let row = 0; row < HEIGHT; row++){
-        for (let col = 0; col < WIDTH; col++){
+
+    //update the grid with cells from newGrid
+    for (let row = 0; row < HEIGHT; row++) {
+        for (let col = 0; col < WIDTH; col++) {
             grid[row][col] = nextGrid[row][col];
         }
     }
 }
 
-export { grid }
+//make grid and next() function available from other files
+export {grid, next}
--- ex4/presenter.js
+++ ex4/presenter.js
@@ -5,21 +5,23 @@ import {grid, next} from "./model.js";
 const gameDiv = document.getElementById("game");
 const nextButton = document.getElementById("nextButton");
 
+//create the required checkboxes
 for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[0].length; j++) {
         const cell = document.createElement("input");
         cell.setAttribute("type", "checkbox");
         cell.checked = false;
-        cell.setAttribute("id", "checkbox-x" + String(i).padStart(2,"0") + "y" + String(j).padStart(2,"0"));
+        cell.setAttribute("id", "checkbox-x" + String(i).padStart(2, "0") + "y" + String(j).padStart(2, "0"));
         gameDiv.appendChild(cell);
     }
+    //add a break tag at the end of a checkmark row
     gameDiv.appendChild(document.createElement("br"))
 }
 
 function updateView() {
-    for (let x = 0; x < grid.length; x++){
-        for (let y = 0; y < grid[0].length; y++){
-            const id = "checkbox-x" + String(x).padStart(2,"0") + "y" + String(y).padStart(2,"0");
+    for (let x = 0; x < grid.length; x++) {
+        for (let y = 0; y < grid[0].length; y++) {
+            const id = "checkbox-x" + String(x).padStart(2, "0") + "y" + String(y).padStart(2, "0");
             const cell = document.getElementById(id);
             cell.checked = grid[x][y];
         }
